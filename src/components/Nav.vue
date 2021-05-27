9daafcbdf8f099402d452283a2780c731e6fc478
pauuukin <oleg.paukin@mail.ru>
2021-05-27 14:32:25 +0300

    init

---
+++ src/components/Nav.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul class="nav justify-content-center">
+    <li class="nav-item">
+      <a class="nav-link active" aria-current="page" href="#">Главная</a
+      >
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#photo">Фотогалерея</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#map">Место и время</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+</script>
+
+
+
+<style scoped>
+</style>
+
